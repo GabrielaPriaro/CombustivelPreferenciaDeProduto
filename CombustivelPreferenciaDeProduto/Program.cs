777b270a637cef2ddce7d607a387c82319c32b52
Gabriela <gabriela.priaro@gmail.com>
2024-10-28 15:16:06 +0000

    Adicionar arquivos de projeto.

---
+++ CombustivelPreferenciaDeProduto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CombustivelPreferenciaDeProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
